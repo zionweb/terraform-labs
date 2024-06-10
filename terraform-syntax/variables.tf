b1912942fbda19e0de2527c7ec7bf70b76e128fd
zionweb <92651245+zionweb@users.noreply.github.com>
2024-06-10 11:20:00 -0300

    terraform syntax lab success

---
+++ terraform-syntax/variables.tf
@@ -0,0 +1,11 @@
+variable "app_name" {
+    description = "Application Name"
+    type        = string
+    default     = "MyApp"
+}
+
+variable "environment_name" {
+    description = "Environment Name"
+    type        = string
+    default     = "Dev"
+}
