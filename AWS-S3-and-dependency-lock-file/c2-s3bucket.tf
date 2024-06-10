849caa09c29e37b322d80650215f423dd7aa6389
zionweb <92651245+zionweb@users.noreply.github.com>
2024-06-10 12:28:38 -0300

    Deploy S3 and dependency lock lab

---
+++ AWS-S3-and-dependency-lock-file/c2-s3bucket.tf
@@ -0,0 +1,11 @@
+# Resource Block: Create Random Pet Name 
+resource "random_pet" "petname" {
+  length    = 5
+  separator = "-"
+}
+
+# Resource Block: Create AWS S3 Bucket
+resource "aws_s3_bucket" "sample" {
+  bucket = random_pet.petname.id
+  region = "us-east-1"  # Comment this if we are going to use AWS Provider v3.x version
+}
